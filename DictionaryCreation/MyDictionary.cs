e6fd411d2b735677f40b9f18551c7a0ea16f6d26
Ege Çıtak <egecitak01@gmail.com>
2021-01-31 13:11:43 +0300

    Add project files.

---
+++ DictionaryCreation/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryCreation
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TKey[] tempKeys;
+        TValue[] values;
+        TValue[] tempValues;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        
+        public void Add(TKey tKey, TValue tValue)
+        {
+            tempKeys = keys;
+            keys = new TKey[keys.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = tKey;
+
+            tempValues = values;
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            values[values.Length - 1] = tValue;
+        }
+        public TKey[] Keys
+        {
+            get { return keys; }
+        }
+        public TValue[] Values
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ DictionaryCreation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DictionaryCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Ege");
+            myDictionary.Add(2, "Engin");
+            myDictionary.Add(3, "Ahmet");
+            myDictionary.Add(4, "Kaan");
+
+            for (int i = 0; i < myDictionary.Keys.Length; i++)
+            {
+                Console.WriteLine(myDictionary.Keys[i] + "- " + myDictionary.Values[i]);
+            }
+        }
+    }
+}
